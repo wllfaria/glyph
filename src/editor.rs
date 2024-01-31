c091d970e19b873a27f34de01f033d0d30c48123
Willians Faria <dev.willians.faria@gmail.com>
2024-01-31 01:12:48 -0300

    feat: limiting fps to 60

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,5 @@
 use std::io::{stdout, Result, Write};
+use std::{thread, time};
 
 use crate::{
     command::{Command, EditorCommands},
@@ -23,8 +24,12 @@ impl Editor {
 
     pub fn start(&mut self) -> Result<()> {
         self.view.initialize()?;
+        let target_fps = 60;
+        let target_frame_time = time::Duration::from_millis(1000 / target_fps);
 
         while self.is_running {
+            let frame_start = time::Instant::now();
+
             match self.events.poll()? {
                 Some(Command::Editor(EditorCommands::Quit)) => self.is_running = false,
                 Some(command) => self.view.handle_command(command),
@@ -32,6 +37,11 @@ impl Editor {
             }
             self.view.render()?;
             stdout().flush()?;
+
+            let frame_duration = frame_start.elapsed();
+            if frame_duration < target_frame_time {
+                thread::sleep(target_frame_time - frame_duration);
+            }
         }
 
         self.view.shutdown()?;
