f589f3d1aef6e3c83521f48bd98bf34c06681580
Willians Faria <dev.willians.faria@gmail.com>
2024-02-05 02:54:33 -0300

    feat: first implementation of a scrolling view

--- src/pane/pane.rs
+++ src/pane/pane.rs
@@ -5,7 +5,7 @@ use std::rc::Rc;
 
 use crate::buffer::Buffer;
 use crate::command::{Command, CursorCommands, EditorCommands};
-use crate::config::{Config, LineNumbers};
+use crate::config::Config;
 use crate::cursor::Cursor;
 use crate::pane::{LineDrawer, PaneDimensions, Position};
 
@@ -60,8 +60,22 @@ impl Pane {
     fn handle_cursor_command(&mut self, command: Command) -> Result<()> {
         self.cursor.handle(&command, &self.buffer.borrow().lines);
         match command {
-            Command::Cursor(CursorCommands::MoveUp) => self.maybe_redraw_sidebar()?,
-            Command::Cursor(CursorCommands::MoveDown) => self.maybe_redraw_sidebar()?,
+            Command::Cursor(CursorCommands::MoveUp) => {
+                if self.cursor.position.row < self.scroll.row {
+                    self.scroll.row = self.cursor.position.row;
+                    self.clear_buffer()?;
+                    self.draw_buffer()?;
+                }
+                self.maybe_redraw_sidebar()?;
+            }
+            Command::Cursor(CursorCommands::MoveDown) => {
+                if self.cursor.position.row == self.scroll.row + self.dimensions.height {
+                    self.scroll.row += 1;
+                    self.clear_buffer()?;
+                    self.draw_buffer()?;
+                }
+                self.maybe_redraw_sidebar()?;
+            }
             _ => (),
         }
         self.draw_cursor()?;
@@ -70,17 +84,15 @@ impl Pane {
 
     fn draw_cursor(&mut self) -> Result<()> {
         let col = self.cursor.position.col + self.config.sidebar_width + self.config.sidebar_gap;
-        self.stdout
-            .queue(cursor::MoveTo(col, self.cursor.position.row))?;
+        self.stdout.queue(cursor::MoveTo(
+            col,
+            self.cursor.position.row - self.scroll.row,
+        ))?;
         Ok(())
     }
 
     fn maybe_redraw_sidebar(&mut self) -> Result<()> {
-        match self.config.line_numbers {
-            LineNumbers::Absolute => (),
-            LineNumbers::None => (),
-            _ => self.draw_sidebar()?,
-        }
+        self.draw_sidebar()?;
         Ok(())
     }
 
@@ -105,13 +117,25 @@ impl Pane {
         Ok(())
     }
 
+    fn clear_buffer(&mut self) -> Result<()> {
+        let offset = self.config.sidebar_width + self.config.sidebar_gap;
+        for row in 0..self.dimensions.height {
+            for col in offset..self.dimensions.width {
+                self.stdout
+                    .queue(cursor::MoveTo(col, row))?
+                    .queue(Print(" "))?;
+            }
+        }
+        Ok(())
+    }
+
     fn draw_buffer(&mut self) -> Result<()> {
         let lines = &self.buffer.borrow().lines[self.scroll.row as usize..];
         let offset = self.dimensions.col + self.config.sidebar_width + self.config.sidebar_gap;
         let total_lines = self.dimensions.height.min(lines.len() as u16);
         for row in 0..total_lines {
             let line = &lines[row as usize];
-            let len = self.dimensions.width.min(line.len() as u16);
+            let len = u16::min(self.dimensions.width - offset, line.len() as u16);
             let line = line[0..len as usize].to_string();
             self.stdout
                 .queue(cursor::MoveTo(offset, row as u16))?
