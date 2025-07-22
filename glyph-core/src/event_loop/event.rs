2d611b0b20b2848d5422f9cb05a6a8f9d84cc744
Willians Faria <dev.willians.faria@gmail.com>
2025-07-22 00:10:41 -0300

    better handling of key presses for keymaps

--- glyph-core/src/event_loop/event.rs
+++ glyph-core/src/event_loop/event.rs
@@ -17,6 +17,69 @@ pub struct KeyEvent {
     pub kind: KeyEventKind,
 }
 
+impl std::fmt::Display for KeyEvent {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.code {
+            KeyCode::Char(c) => write!(f, "{c}"),
+            KeyCode::Backspace => write!(f, "<bs>"),
+            KeyCode::Enter => write!(f, "<cr>"),
+            KeyCode::Left => write!(f, "<left>"),
+            KeyCode::Right => write!(f, "<right>"),
+            KeyCode::Up => write!(f, "<up>"),
+            KeyCode::Down => write!(f, "<down>"),
+            KeyCode::Home => write!(f, "<home>"),
+            KeyCode::End => write!(f, "<end>"),
+            KeyCode::PageUp => write!(f, "<pgup>"),
+            KeyCode::PageDown => write!(f, "<pgdn>"),
+            KeyCode::Tab => write!(f, "<tab>"),
+            KeyCode::BackTab => write!(f, "<btab>"),
+            KeyCode::Delete => write!(f, "<del>"),
+            KeyCode::Insert => write!(f, "<ins>"),
+            KeyCode::F(n) => write!(f, "<f{n}>"),
+            KeyCode::Null => write!(f, "<null>"),
+            KeyCode::Esc => write!(f, "<esc>"),
+            KeyCode::CapsLock => write!(f, "<caps>"),
+            KeyCode::ScrollLock => write!(f, "<scroll>"),
+            KeyCode::NumLock => write!(f, "<num>"),
+            KeyCode::PrintScreen => write!(f, "<print>"),
+            KeyCode::Pause => write!(f, "<pause>"),
+            KeyCode::Menu => write!(f, "<menu>"),
+            KeyCode::KeypadBegin => write!(f, "<begin>"),
+            KeyCode::Media(media) => match media {
+                MediaKeyCode::Play => write!(f, "<play>"),
+                MediaKeyCode::Pause => write!(f, "<pause>"),
+                MediaKeyCode::PlayPause => write!(f, "<playpause>"),
+                MediaKeyCode::Reverse => write!(f, "<reverse>"),
+                MediaKeyCode::Stop => write!(f, "<stop>"),
+                MediaKeyCode::FastForward => write!(f, "<ffwd>"),
+                MediaKeyCode::Rewind => write!(f, "<rewind>"),
+                MediaKeyCode::TrackNext => write!(f, "<next>"),
+                MediaKeyCode::TrackPrevious => write!(f, "<prev>"),
+                MediaKeyCode::Record => write!(f, "<record>"),
+                MediaKeyCode::LowerVolume => write!(f, "<vol-down>"),
+                MediaKeyCode::RaiseVolume => write!(f, "<vol-up>"),
+                MediaKeyCode::MuteVolume => write!(f, "<mute>"),
+            },
+            KeyCode::Modifier(modifier) => match modifier {
+                ModifierKeyCode::LeftShift => write!(f, "<shift>"),
+                ModifierKeyCode::LeftControl => write!(f, "<ctrl>"),
+                ModifierKeyCode::LeftAlt => write!(f, "<alt>"),
+                ModifierKeyCode::LeftSuper => write!(f, "<super>"),
+                ModifierKeyCode::LeftHyper => write!(f, "<hyper>"),
+                ModifierKeyCode::LeftMeta => write!(f, "<meta>"),
+                ModifierKeyCode::RightShift => write!(f, "<shift>"),
+                ModifierKeyCode::RightControl => write!(f, "<ctrl>"),
+                ModifierKeyCode::RightAlt => write!(f, "<alt>"),
+                ModifierKeyCode::RightSuper => write!(f, "<super>"),
+                ModifierKeyCode::RightHyper => write!(f, "<hyper>"),
+                ModifierKeyCode::RightMeta => write!(f, "<meta>"),
+                ModifierKeyCode::IsoLevel3Shift => write!(f, "<shift>"),
+                ModifierKeyCode::IsoLevel5Shift => write!(f, "<shift>"),
+            },
+        }
+    }
+}
+
 #[derive(Debug, PartialOrd, Ord, PartialEq, Eq, Clone, Copy, Hash)]
 pub enum KeyCode {
     Backspace,
